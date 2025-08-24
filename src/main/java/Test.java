a093754a22c45cb7eb576d6d6dce787912188fc7
Sayat86 <s.beysembayev@bk.ru>
2025-08-24 17:14:54 +0600

    added solutions

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -18,12 +18,39 @@ public class Test {
 //            System.out.println(video.getId() + "." + video.getName() + " " + video.getCreatedAt());
 //        }
 
+//        User user = em.find(User.class, 1);
+//        System.out.println("Пользователь: " + user.getName());
+//        System.out.println("Подписки: ");
+//
+//        for (Channel channel : user.getSubscribedChannels()) {
+//            System.out.println(" - " + channel.getName() + " [" + channel.getVideos().size() + " videos]");
+//        }
+
+//        Video video = em.find(Video.class, 1);
+//        System.out.println("Видео: " + video.getName());
+//        System.out.println("Опубликовано: " + video.getCreatedAt());
+//        System.out.println("Канал: " + video.getChannel().getName());
+//        System.out.println("Просмотры: " + video.getViews());
+//        System.out.println("Количество унмкальных просмотров: " + video.getId());
+//
+//        Channel channel = em.find(Channel.class, 1);
+//        System.out.println("Канал: " + channel.getName());
+//        System.out.println("Страна: " + channel.getCountry());
+//        System.out.println("Владелец канала: " + channel.getUser().getName());
+//        System.out.println("Дата создания: " + channel.getCreatedAt());
+//        System.out.println("Видео:");
+//        for (Video video : channel.getVideos()) {
+//            System.out.println(video.getId() + "." + video.getName() + " [" + video.getViews() + " views]");
+//        }
+
         User user = em.find(User.class, 1);
         System.out.println("Пользователь: " + user.getName());
-        System.out.println("Подписки: ");
-
+        System.out.println("Подписки:");
         for (Channel channel : user.getSubscribedChannels()) {
-            System.out.println(" - " + channel.getName() + " [" + channel.getVideos().size() + " videos]");
+            System.out.println(" - " + channel.getName());
+            for (Video video : channel.getVideos()) {
+                System.out.println("     " + video.getId() + ". " + video.getName() + " [" + video.getViews() + " views]");
+            }
         }
 
 
