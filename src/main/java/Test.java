a023dac5a3becb2fb66cd4090d3414bb4d43d08f
runtime-1 <runtimestudent2023@gmail.com>
2025-08-25 13:33:24 +0500

    correct

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,6 +5,9 @@ import model.Channel;
 import model.User;
 import model.Video;
 
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class Test {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
@@ -26,12 +29,13 @@ public class Test {
 //            System.out.println(" - " + channel.getName() + " [" + channel.getVideos().size() + " videos]");
 //        }
 
-//        Video video = em.find(Video.class, 1);
-//        System.out.println("Видео: " + video.getName());
-//        System.out.println("Опубликовано: " + video.getCreatedAt());
-//        System.out.println("Канал: " + video.getChannel().getName());
-//        System.out.println("Просмотры: " + video.getViews());
-//        System.out.println("Количество унмкальных просмотров: " + video.getId());
+        Video video = em.find(Video.class, 1);
+        System.out.println("Видео: " + video.getName());
+        System.out.println("Опубликовано: " + video.getCreatedAt().getDayOfMonth() +
+                 " " + video.getCreatedAt().getMonth().getDisplayName(TextStyle.FULL, new Locale("ru")));
+        System.out.println("Канал: " + video.getChannel().getName());
+        System.out.println("Просмотры: " + video.getViews());
+        System.out.println("Количество уникальных просмотров: " + video.getViewList().size());
 //
 //        Channel channel = em.find(Channel.class, 1);
 //        System.out.println("Канал: " + channel.getName());
@@ -43,15 +47,15 @@ public class Test {
 //            System.out.println(video.getId() + "." + video.getName() + " [" + video.getViews() + " views]");
 //        }
 
-        User user = em.find(User.class, 1);
-        System.out.println("Пользователь: " + user.getName());
-        System.out.println("Подписки:");
-        for (Channel channel : user.getSubscribedChannels()) {
-            System.out.println(" - " + channel.getName());
-            for (Video video : channel.getVideos()) {
-                System.out.println("     " + video.getId() + ". " + video.getName() + " [" + video.getViews() + " views]");
-            }
-        }
+//        User user = em.find(User.class, 1);
+//        System.out.println("Пользователь: " + user.getName());
+//        System.out.println("Подписки:");
+//        for (Channel channel : user.getSubscribedChannels()) {
+//            System.out.println(" - " + channel.getName());
+//            for (Video video : channel.getVideos()) {
+//                System.out.println("     " + video.getId() + ". " + video.getName() + " [" + video.getViews() + " views]");
+//            }
+//        }
 
 
         em.close();
